221a1b815940f8a83e3e4903532960b90be8c3cc
vivan raj <vivanrajmittakodi6@gmail.com>
2023-08-16 18:22:59 +0530

    updated

--- app.js
+++ app.js
@@ -9,10 +9,30 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
 
+function moreAboutKaren(parents,noofsibliings,isNuclearFamily){
+    if(typeof parents == 'string'&& typeof noofsibliings == 'number'&& typeof isNuclearFamily=='boolean'){
+        return true
+    }
+    else{
+
+        return false
+    }
+}
+
 //Trial 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+
+function doesFriendExist(x,y){
+    if (isNaN(x)){
+       return x
+
+}
+else {
+    return y
+}
+}
 
 //Trial 4:
 //Lily gave Karen x sweets
@@ -21,9 +41,23 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
 
+function sweetTooth(x,y,z,n){
+    return ((x-y)-z*n)/2
+}
+
 //Trial 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+
+ function convertToCelcius (x){  
+    return (x-32)*(5/9)
+
+ }
+
+
+
+
+
 
 //Trial 6:
 //Lily can now do multiple things to deal with this
@@ -33,6 +67,33 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
 
+ function aDifficultChoice (v){
+    if(v == 1){
+        return "Take her daughter to a doctor";
+    }
+    else  if(v == 2){
+        return "Talk to her husband about it";
+    }
+ 
+    else if(v == 3){
+        return "Counsel her daughter herself";
+    }
+    else {    
+        return "Lock her daughter in her room";
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
 //Challenge 1:
 //Lily realized that she'd hurt her daughter
 //All she wants now is for her to stop crying
@@ -41,3 +102,18 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Take all of Lily's strategies and concatenate them to a single var
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
+
+
+function consoleKaren (arr){
+ var temp = ""
+  for( let i=0; i<arr.length; i++){
+    temp = temp + " " + arr[i]
+  }  
+    return temp.length-1 
+}
+
+
+
+
+
+
